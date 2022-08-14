0c4c40da61bca39c136033378c178a7336d25e31
Christian Salinas <ChristianSalinas1009@gmail.com>
2022-08-14 11:48:22 -0400

    fixed broken images

--- controllers/places.js
+++ controllers/places.js
@@ -1,19 +1,7 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
-router.get('/', (req, res) => {
-  let places = [{
-    name: "Robin's Place",
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/rest1.avif'
-  }, {
-    name: "Christian's Borgar Joint",
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/res2.avif'
-  }]
+router.get('/', (req, res) =>{
   res.render('places/index', { places })
 })
 
@@ -23,8 +11,20 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
 
 
 
---
+++ models/places.js
@@ -0,0 +1,14 @@
+
+module.exports = [{
+    name: "Robin's Burgers",
+    city: 'Belleville',
+    state: 'NJ',
+    cuisines: 'Burgers',
+    pic: '/images/rest1.avif'
+}, {
+    name: "Christian's Cafe",
+    city: 'Rochester',
+    state: 'NH',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/res2.avif'
+}]
